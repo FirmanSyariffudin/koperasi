7d39faff974ed9d5e9ffd5b7f041784facedcee9
RDP77 <ravidwiputra@gmail.com>
2021-07-04 02:49:11 +0700

    Clean Model And Fixing Variabel

--- resources/views/layouts/components/nav.blade.php
+++ resources/views/layouts/components/nav.blade.php
@@ -11,8 +11,7 @@
                 <img alt="image" src="{{ asset('avatar.png') }}" class="rounded-circle mr-1">
                 <div class="d-sm-none d-lg-inline-block">{{ __('Hai, ') . Auth::user()->name }}</div>
             </a>
-            <div class="dropdown-menu dropdown-menu-right">
-                <div class="dropdown-title">{{ Auth::user()->relationRoles->name }}</div>
+            <div class="dropdown-menu dropdown-menu-right">2
                 <a id="name" class="dropdown-item has-icon" style="cursor: pointer">
                     <i class="fas fa-user"></i> {{ __('Ganti Nama') }}
                 </a>
